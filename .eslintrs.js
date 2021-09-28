a6a95979a8c1031912b437bd92873c65e97aed7c
Aleksei Iudin <iudin_a@yahoo.com>
2021-09-28 23:12:24 +0300

    lesson1: global objects

---
+++ .eslintrs.js
@@ -0,0 +1,33 @@
+module.exports = {
+  extends: [
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
+  ],
+  plugins: ['@typescript-eslint', 'prettier'],
+  env: {
+    browser: false,
+    es6: true,
+  },
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
+  },
+};
---
+++ index.js
@@ -0,0 +1,3 @@
+const userObj = require('./user');
+
+userObj.sayHello();
