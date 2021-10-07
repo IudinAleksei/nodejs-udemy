9d18854f1fdd429f0d73db59298668dc2de5afbf
Aleksei Iudin <iudin_a@yahoo.com>
2021-10-07 22:14:54 +0300

    feat: create simple web-server

--- index.js
+++ index.js
@@ -1,3 +1,55 @@
-const userObj = require('./user');
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
-userObj.sayHello();
+const server = http.createServer((req, res) => {
+  if (req.method === 'GET') {
+    res.writeHead(200, {
+      'Content-Type': 'text/html; charset=utf-8',
+    });
+
+    if (req.url === '/') {
+      fs.readFile(path.join(__dirname, 'views', 'index.html'), 'utf-8', (err, content) => {
+        if (err) {
+          throw err;
+        }
+        res.end(content);
+      });
+      return;
+    }
+
+    if (req.url === '/about') {
+      fs.readFile(path.join(__dirname, 'views', 'about.html'), 'utf-8', (err, content) => {
+        if (err) {
+          throw err;
+        }
+        res.end(content);
+      });
+      return;
+    }
+    return;
+  }
+
+  if (req.method === 'POST') {
+    const body = [];
+    req.on('data', (data) => {
+      body.push(Buffer.from(data));
+    });
+
+    req.on('end', () => {
+      const message = body.toString().split('=')[1];
+
+      res.writeHead(200, {
+        'Content-Type': 'text/html; charset=utf-8',
+      });
+      res.end(`
+          <h1>Ваше сообщение: ${message}</h1>
+      `);
+    });
+    return;
+  }
+});
+
+server.listen(3000, () => {
+  console.log('server is running');
+});
